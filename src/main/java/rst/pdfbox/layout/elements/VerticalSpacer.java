4b3a034ef86620c43f86f00bb84053d50f6629cc
Ralf Stuckert <ralf.stuckert@googlemail.com>
2016-03-24 15:25:45 +0100

    added VerticalSpacer

---
+++ src/main/java/rst/pdfbox/layout/elements/VerticalSpacer.java
@@ -0,0 +1,43 @@
+package rst.pdfbox.layout.elements;
+
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+
+import rst.pdfbox.layout.Coords;
+
+public class VerticalSpacer implements DrawableElement, Dividable {
+
+	private float height;
+
+	public VerticalSpacer(float height) {
+		this.height = height;
+	}
+
+	@Override
+	public float getWidth() throws IOException {
+		return 0;
+	}
+
+	@Override
+	public float getHeight() throws IOException {
+		return height;
+	}
+
+	@Override
+	public Coords getAbsolutePosition() {
+		return null;
+	}
+
+	@Override
+	public void draw(PDPageContentStream contentStream, Coords origin)
+			throws IOException {
+		// nothing to draw
+	}
+
+	@Override
+	public Divided divide(float maxHeight) throws IOException {
+		return new Divided(new VerticalSpacer(maxHeight), new VerticalSpacer(
+				getHeight() - maxHeight));
+	}
+}
