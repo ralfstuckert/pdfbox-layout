8243f18d9ca287924c496ff49c2b565673a250f2
Ralf Stuckert <ralf.stuckert@googlemail.com>
2016-04-05 16:44:37 +0200

    introduce explicit compatibility layer

--- pdfbox2-layout/compatibility/rst/pdfbox/layout/util/CompatibilityHelper.java
+++ pdfbox2-layout/compatibility/rst/pdfbox/layout/util/CompatibilityHelper.java
@@ -1,11 +1,12 @@
 package rst.pdfbox.layout.util;
 
-import java.awt.geom.PathIterator;
 import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
+import org.apache.pdfbox.util.Matrix;
 
 /**
  * Provide compatible methods for API changes from pdfbox 1x to 2x.
@@ -14,23 +15,23 @@ public class CompatibilityHelper {
 
     public static void clip(final PDPageContentStream contentStream)
 	    throws IOException {
-	contentStream.clipPath(PathIterator.WIND_NON_ZERO);
+	contentStream.clip();
     }
 
     public static void showText(final PDPageContentStream contentStream, final String text)
 	    throws IOException {
-	contentStream.drawString(text);
+	contentStream.showText(text);
     }
     
     public static void setTextTranslation(
 	    final PDPageContentStream contentStream, final float x,
 	    final float y) throws IOException {
-	contentStream.setTextTranslation(x, y);
+        contentStream.setTextMatrix(Matrix.getTranslateInstance(x, y));
     }
 
     public static PDPageContentStream createAppendablePDPageContentStream(
 	    final PDDocument pdDocument, final PDPage page) throws IOException {
-	return new PDPageContentStream(pdDocument, page, true, true);
+	return new PDPageContentStream(pdDocument, page, AppendMode.APPEND, true);
     }
 
 }
