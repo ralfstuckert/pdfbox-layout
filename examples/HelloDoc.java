ec9daaadca6cf1ae1b030b9f2e2bbf301cf7847a
Ralf Stuckert <ralf.stuckert@googlemail.com>
2016-04-10 21:06:30 +0200

    added hellodoc example

---
+++ examples/HelloDoc.java
@@ -0,0 +1,26 @@
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+import rst.pdfbox.layout.elements.Document;
+import rst.pdfbox.layout.elements.Paragraph;
+import rst.pdfbox.layout.elements.render.VerticalLayoutHint;
+import rst.pdfbox.layout.text.Alignment;
+import rst.pdfbox.layout.text.Constants;
+
+public class HelloDoc {
+
+    public static void main(String[] args) throws Exception {
+	Document document = new Document(Constants.A4, 40, 60, 40, 60);
+
+	Paragraph paragraph = new Paragraph();
+	paragraph.addText("Hello Document", 20,
+		PDType1Font.HELVETICA);
+	document.add(paragraph);
+
+	final OutputStream outputStream = new FileOutputStream("hellodoc.pdf");
+	document.save(outputStream);
+
+    }
+}
