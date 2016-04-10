d5cac7863c6f25fe779aeafaa6c5661fc0b1bd48
Ralf Stuckert <ralf.stuckert@googlemail.com>
2016-04-10 20:50:29 +0200

    fixed vertical offset on start

--- src/main/java/rst/pdfbox/layout/elements/render/ColumnLayout.java
+++ src/main/java/rst/pdfbox/layout/elements/render/ColumnLayout.java
@@ -2,6 +2,8 @@ package rst.pdfbox.layout.elements.render;
 
 import java.io.IOException;
 
+import rst.pdfbox.layout.elements.Drawable;
+
 /**
  * The column layout divides the page vertically into columns. You can specify
  * the number of columns and the inter-column spacing. The layouting inside a
@@ -13,6 +15,8 @@ public class ColumnLayout extends VerticalLayout {
     private final int columnCount;
     private float columnSpacing;
     private int columnIndex = 0;
+    private Float offsetY = null;
+    
 
     public ColumnLayout(int columnCount) {
 	this(columnCount, 0);
@@ -35,12 +39,21 @@ public class ColumnLayout extends VerticalLayout {
 	if (++columnIndex >= columnCount) {
 	    renderContext.newPage();
 	    columnIndex = 0;
+	    offsetY = 0f;
 	} else {
 	    float nextColumnX = (getTargetWidth(renderContext) + columnSpacing)
 		    * columnIndex;
 	    renderContext.resetPositionToUpperLeft();
-	    renderContext.movePositionBy(nextColumnX, 0);
+	    renderContext.movePositionBy(nextColumnX, -offsetY);
 	}
     }
 
+    @Override
+    public void render(RenderContext renderContext, Drawable drawable,
+            LayoutHint layoutHint) throws IOException {
+	if (offsetY == null) {
+	    offsetY = renderContext.getUpperLeft().getY() - renderContext.getCurrentPosition().getY();
+	}
+        super.render(renderContext, drawable, layoutHint);
+    }
 }
