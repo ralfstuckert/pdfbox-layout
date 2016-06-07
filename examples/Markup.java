b7aaa85191bbc465bb99206dec16df119e9da1c2
Ralf Stuckert <ralf.stuckert@googlemail.com>
2016-06-07 07:00:23 +0200

    added indention/list exammples to markup

--- examples/Markup.java
+++ examples/Markup.java
@@ -35,10 +35,32 @@ public class Markup {
 	            "And now also {color:#ff0000}c{color:#00ff00}o{color:#0000ff}l{color:#00cccc}o{color:#cc00cc}r{color:#000000}.\n\n",
 	            11, BaseFont.Times);
 	paragraph.addMarkup(
-		"Escape \\* with \\\\\\* and \\_ with \\\\\\_ in markup.\n",
+		"Escape \\* with \\\\\\* and \\_ with \\\\\\_ in markup.\n\n",
 		11, BaseFont.Times);
 	document.add(paragraph, new VerticalLayoutHint(Alignment.Left, 0, 0,
 		30, 0));
+
+	paragraph = new Paragraph();
+	text1 = "\nAlso, you can do all that indention stuff much easier with markup, e.g. simple indention\n"
+		+ "--At vero eos et accusam\n\n"
+		+ "-!And end the indention. Now a list:\n"
+		+ "-+This is a list item\n"
+		+ "-+Another list item\n"
+		+ " -+A sub list item\n"
+		+ "-+And yet another one\n\n"
+		+ "-!Even enumeration is supported:\n"
+		+ "-#This is a list item\n"
+		+ "-#Another list item\n"
+		+ " -#{a:}A sub list item\n"
+		+ "-#And yet another one\n\n"
+		+ "-!And you can customize it:\n"
+		+ "-#{I ->:5}This is a list item\n"
+		+ "-#{I ->:5}Another list item\n"
+		+ " -#{a ~:30pt}A sub list item\n"
+		+ "-#{I ->:5}And yet another one\n\n";
+	paragraph.addMarkup(text1, 11, BaseFont.Times);
+	document.add(paragraph);
+
 
 	final OutputStream outputStream = new FileOutputStream("markup.pdf");
 	document.save(outputStream);
